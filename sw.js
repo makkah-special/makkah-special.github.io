378d8a5550a274f3ec48daef3f0ff289ac651d80
makkah-special <s3aad47@gmail.com>
2026-06-21 05:42:37 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+// Service worker cleanup for GitHub Pages.
+// This file intentionally clears old caches and unregisters the previous service worker.
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll({ type: 'window', includeUncontrolled: true }))
+      .then((clients) => {
+        clients.forEach((client) => {
+          if (client.url) client.navigate(client.url);
+        });
+      })
+  );
+});
